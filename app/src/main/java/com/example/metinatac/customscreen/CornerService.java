ab5464f2479263d693fbe0ff1612ffbf3999df99
Alpay <cyberfacilitygroup@gmail.com>
2019-01-12 02:53:05 +0100

    APP Fertig

---
+++ app/src/main/java/com/example/metinatac/customscreen/CornerService.java
@@ -0,0 +1,85 @@
+package com.example.metinatac.customscreen;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class CornerService extends Service {
+    public CornerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+
+        ImageView ivTopLeft = new ImageView(this);
+        ImageView ivTopRight = new ImageView(this);
+        ImageView ivBottomLeft = new ImageView(this);
+        ImageView ivBottomRight = new ImageView(this);
+
+        ImageView ivNotch = new ImageView(this);
+
+        ivTopLeft.setImageResource(R.drawable.ic_corner);
+        ivTopRight.setImageResource(R.drawable.ic_corner);
+        ivBottomLeft.setImageResource(R.drawable.ic_corner);
+        ivBottomRight.setImageResource(R.drawable.ic_corner);
+        ivNotch.setImageResource(R.drawable.ic_notch);
+
+        ivTopRight.setRotation(90);
+        ivBottomLeft.setRotation(-90);
+        ivBottomRight.setRotation(180);
+
+
+        //Creating Params
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+
+                50,50,      //Width and height
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,     // Type
+
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+                | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+                | WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                | WindowManager.LayoutParams.FLAG_LAYOUT_ATTACHED_IN_DECOR,
+
+                PixelFormat.TRANSLUCENT     //Format
+
+        );
+
+        //Add Views onto Screen:
+        params.gravity = Gravity.TOP | Gravity.LEFT;
+        manager.addView(ivTopLeft, params);
+
+        params.gravity = Gravity.TOP | Gravity.RIGHT;
+        manager.addView(ivTopRight, params);
+
+        params.gravity = Gravity.BOTTOM | Gravity.LEFT;
+        manager.addView(ivBottomLeft, params);
+
+        params.gravity = Gravity.BOTTOM | Gravity.RIGHT;
+        manager.addView(ivBottomRight, params);
+
+        params.gravity = Gravity.CENTER | Gravity.TOP;
+        ivNotch.setScaleType(ImageView.ScaleType.FIT_XY);
+        params.width = 450;
+        params.height = 75;
+        manager.addView(ivNotch, params);
+
+    }
+}
--- app/src/main/java/com/example/metinatac/customscreen/MainActivity.java
+++ app/src/main/java/com/example/metinatac/customscreen/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.metinatac.customscreen;
 
+import android.app.Service;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int OVERLAY_PERMISSION_CODE =2002 ; // can be anything
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        checkPremission();
+    }
+
+
+
+    public void checkPremission() {
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!Settings.canDrawOverlays(this)) {
+
+                // Open the permission page
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_CODE);
+                return;
+            }
+        }
+
+
+        goToReactActivity();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+
+        // If the permission has been checked
+        if (requestCode == OVERLAY_PERMISSION_CODE) {
+            if (Settings.canDrawOverlays(this)) {
+                goToReactActivity();
+            }
+        }
+    }
+
+    private void goToReactActivity() {
+        startService(new Intent(this, CornerService.class));
+        finish();
     }
 }
